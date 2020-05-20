9939e862e25808168a2a93fc9c918fc0f063ab27
bairning <bairn01@163.com>
2020-05-20 17:38:31 +0800

    finished

---
+++ server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+	"time"
+	"websocket/impl"
+)
+import "github.com/gorilla/websocket"
+
+var (
+	upgrader = websocket.Upgrader{
+		//允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler (w http.ResponseWriter, r *http.Request) {
+	var (
+		wsConn *websocket.Conn
+		err error
+		conn *impl.Connection
+		data []byte
+	)
+
+	if wsConn, err = upgrader.Upgrade(w, r, nil);err != nil {
+		return
+	}
+
+	if conn, err = impl.InitConnection(wsConn); err != nil {
+		goto ERR
+	}
+
+	go func() {
+		var (err error)
+		for {
+			if err = conn.WriteMessage([]byte("heartbeet"));err != nil {
+				return
+			}
+			time.Sleep(1*time.Second)
+		}
+	}()
+
+	for {
+		if data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(data); err != nil {
+			goto ERR
+		}
+	}
+
+	ERR:
+		conn.Close()
+}
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe("0.0.0.0:7777", nil)
+}
